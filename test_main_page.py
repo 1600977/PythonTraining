eb7d8322d5a77c66083a38fc61e314cd902979fc
1600977 <1600977@gmail.com>
2023-06-12 13:15:53 -0400

    check wrong password

--- test_main_page.py
+++ test_main_page.py
@@ -16,7 +16,7 @@ def driver(get_chrome_options):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
 
-
+@pytest.mark.skip('correct test')
 def test_succefull_autotization(driver):
     driver.get('https://www.saucedemo.com/')   
     username = driver.find_element(By.XPATH,'//input[@data-test="username"]') 
@@ -28,6 +28,17 @@ def test_succefull_autotization(driver):
     sleep(5)
     products_title = driver.find_element(By.XPATH,'//span[@class="title"]') 
     assert products_title.text == 'Products'#helolo
+    driver.close()
 
-def test_hello():
-    assert 1 == 1 
+def test_wrong_password(driver):
+    driver.get('https://www.saucedemo.com/')
+    username = driver.find_element(By.XPATH,'//input[@data-test="username"]')
+    username.send_keys('locked_out_user')
+    password = driver.find_element(By.XPATH,'//input[@data-test="password"]')
+    password.send_keys('secret_sauce')
+    login = driver.find_element(By.XPATH,'//input[@id="login-button"]')
+    login.click()
+    error_title = driver.find_element(By.XPATH,'//h3[@data-test="error"]')
+    assert error_title.text == 'Epic sadface: Sorry, this user has been locked out.'
+    driver.close()
+
